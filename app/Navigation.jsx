fd79f9622045ba42ff76bc6327c740d52ab908be
JustSomeRandomKid <127970878+JustSomeRandomKid@users.noreply.github.com>
2025-07-26 18:36:14 +0300

    Add Messenger screen and update navigation
    Introduces a new Messenger screen with real-time chat functionality using Appwrite and adds it to the navigation stack. The Messenger component manages chat history, handles sending messages, and subscribes to real-time updates for a specified NGO collection.

--- app/Navigation.jsx
+++ app/Navigation.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignupScreen from './screens/Signup';
-import LoginScreen from './screens/Login';
 import MainContainer from './MainContainer'; // Import the tab navigator
+import LoginScreen from './screens/Login';
+import messenger from './screens/Messenger';
+import SignupScreen from './screens/Signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +12,7 @@ const Navigation = () => {
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Main" component={MainContainer} options={{ headerShown: false }} />
+      <Stack.Screen name="Messenger" component={messenger} />
     </Stack.Navigator>
   );
 };
